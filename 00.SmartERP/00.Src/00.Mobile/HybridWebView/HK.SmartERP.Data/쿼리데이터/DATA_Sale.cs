519998068311aa7b0422b5286cd67fd4558a0334
shin <snaiper1208@naver.com>
2018-12-10 23:58:20 +0900

    --

--- 00.SmartERP/00.Src/00.Mobile/HybridWebView/HK.SmartERP.Data/쿼리데이터/DATA_Sale.cs
+++ 00.SmartERP/00.Src/00.Mobile/HybridWebView/HK.SmartERP.Data/쿼리데이터/DATA_Sale.cs
@@ -8,7 +8,7 @@ namespace HK.SmartERP.Data
     {
         public long SALE_SN { set; get; }
         public string SALE_NAME { set; get; }
-        public long AS_SN { set; get; }
+        public long AC_SN { set; get; }
         public byte[] SALE_ITEMS { set; get; }
         
         public long SALE_UPDATE_SN { set; get; }
