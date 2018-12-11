6de9bb0538405820e57d88e51b74d3d7494ae964
snaiper1208 <snaiper1208@winitech.com>
2018-12-11 15:55:15 +0900

    --

--- 00.SmartERP/00.Src/00.Mobile/HybridWebView/CustomRenderer/Views/00.기초정보관리/page상품관리.xaml.cs
+++ 00.SmartERP/00.Src/00.Mobile/HybridWebView/CustomRenderer/Views/00.기초정보관리/page상품관리.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HK.SmartERP.Data;
+using HK.SmartERP.LIB.Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,50 @@ namespace CustomRenderer.Views
 		public page상품관리 ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            hybridWebView.RegisterAction(data => ShowData(data));
+
+            hybridWebView.BatchBegin();
+
+            hybridWebView.OnLoadPage += onLoadPage;
+        }
+
+        private void ShowData(string str)
+        {
+            var result = 이벤트처리기.이벤트처리(str);
+
+            var 데이터 = JsonTool.Deserialize<이벤트데이터>(result);
+
+            switch (데이터.handle)
+            {
+                case HARDCODE.이벤트.화면호출:
+                    보기_상세관리(데이터.data);
+                    break;
+                case HARDCODE.이벤트.전체조회:
+                    hybridWebView.ShowResult(데이터.data);
+                    break;
+            }
+        }
+
+        void 보기_상세관리(string data)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await Navigation.PushAsync(new page거래처상세관리(data));
+            });
+        }
+
+        private void onLoadPage()
+        {
+            이벤트데이터 데이터 = new 이벤트데이터();
+
+            데이터.view = HARDCODE.화면.상품;
+            데이터.handle = HARDCODE.이벤트.전체조회;
+            데이터.data = string.Empty;
+
+            var data = JsonTool.Serialize(데이터);
+
+            ShowData(data);
+        }
+    }
 }
