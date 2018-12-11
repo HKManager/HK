2600637b639a27dd75fb65209766699e9985e0e4
snaiper1208 <snaiper1208@winitech.com>
2018-12-11 17:25:18 +0900

    --

---
+++ 00.SmartERP/00.Src/00.Mobile/HybridWebView/HK.SmartERP.Data/쿼리/Query_매입.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HK.SmartERP.Data
+{
+    public class Query_매입
+    {
+        public const string GetList = @"
+            SELECT 
+	            BUY_SN
+	            , BUY_NAME
+	            , AC_SN
+	            , BUY_ITEMS
+	            , BUY_REGISTER_DT
+	            , BUY_DESC
+            FROM BUY
+        ";
+
+        public const string Insert = @"
+            INSERT INTO
+                buy
+                (
+                    BUY_NAME
+                    , AC_SN
+                    , BUY_ITEMS
+                    , BUY_REGISTER_DT
+                    , BUY_DESC
+                )
+                VALUES
+                (
+                    '{0}'
+                    , '{1}'
+                    , '{2}'
+                    , '{3}'
+                    , '{4}'
+                )
+        ";
+
+        public const string Update = @"
+            UPDATE buy
+            SET    BUY_NAME = '{0}',
+                   BUY_ITEMS = '{1}',
+                   BUY_REGISTER_DT = '{2}',
+                   BUY_REGISTER_DT = '{3}'
+            WHERE  BUY_SN = {4}
+        ";
+
+
+        public const string Delete = @"
+            DELETE
+            FROM BUY
+            WHERE BUY_SN = {0}
+        ";
+
+        public const string UpdateAddPrice = @"
+			UPDATE account
+            SET    AC_PAY = (
+                AC_PAY + {0}
+            )
+            WHERE  AC_SN = {1}
+        ";
+
+        public const string UpdateSubPrice = @"
+			UPDATE account
+            SET    AC_PAY = (
+                AC_PAY - {0}
+            )
+            WHERE  AC_SN = {1}
+        ";
+    }
+}
