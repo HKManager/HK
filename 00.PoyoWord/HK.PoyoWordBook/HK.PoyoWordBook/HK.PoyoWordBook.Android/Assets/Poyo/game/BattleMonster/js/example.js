09d041ecb5c8415f0b1ac3021562217791f3a978
snaiper1208 <snaiper1208@naver.com>
2019-01-17 23:44:37 +0900

    --

--- 00.PoyoWord/HK.PoyoWordBook/HK.PoyoWordBook/HK.PoyoWordBook.Android/Assets/Poyo/game/BattleMonster/js/example.js
+++ 00.PoyoWord/HK.PoyoWordBook/HK.PoyoWordBook/HK.PoyoWordBook.Android/Assets/Poyo/game/BattleMonster/js/example.js
@@ -2,13 +2,6 @@
   var body = document.body;
   var loading = document.createElement('class');
   loading.className = 'loading';
-  if (Stage._supported) {
-    loading.innerHTML = 'Loading...';
-    loading.style.zIndex = -1;
-  } else {
-    loading.innerHTML = 'Please use a <a target="_blank" href="https://www.google.com/search?q=modern+browser">modern browser!';
-    loading.style.zIndex = 0;
-  }
   body.insertBefore(loading, body.firstChild);
 })();
 
