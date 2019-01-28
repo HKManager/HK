5bdca20ebec3b3c39724a9a0fa5f9d7925ad6f76
shin <snaiper1208@naver.com>
2019-01-29 01:55:07 +0900

    --

--- 00.PoyoWord/HK.PoyoWordBook/HK.PoyoWordBook/HK.PoyoWordBook.Android/Assets/Poyo/game/BattleMonster/pokemon.js
+++ 00.PoyoWord/HK.PoyoWordBook/HK.PoyoWordBook/HK.PoyoWordBook.Android/Assets/Poyo/game/BattleMonster/pokemon.js
@@ -239,3 +239,14 @@ var setactiveans = function(ele)
 		document.querySelector('.active').className = "";
 	ele.className = "active";
 }
+
+var submitAnswer = function() 
+{
+  var answer = document.getElementsByClassName('active')[0].innerText;
+
+
+  alert(answer);
+
+  var abc = 0;
+  // var attempt = (document.getElementsByClassName('active')[0].id == "correctans") ? true : false;
+}
