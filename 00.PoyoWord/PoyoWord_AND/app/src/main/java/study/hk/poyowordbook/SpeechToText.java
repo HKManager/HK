d64f392623d82591e10be2a816c2cbd87ebaaf23
snaiper1208 <snaiper1208@naver.com>
2019-03-13 01:05:12 +0900

    --

---
+++ 00.PoyoWord/PoyoWord_AND/app/src/main/java/study/hk/poyowordbook/SpeechToText.java
@@ -0,0 +1,105 @@
+package study.hk.poyowordbook;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.kakao.sdk.newtoneapi.SpeechRecognizeListener;
+import com.kakao.sdk.newtoneapi.SpeechRecognizerActivity;
+import com.kakao.sdk.newtoneapi.SpeechRecognizerClient;
+import com.kakao.sdk.newtoneapi.SpeechRecognizerManager;
+import com.kakao.sdk.newtoneapi.impl.util.PermissionUtils;
+
+import java.util.ArrayList;
+
+public class SpeechToText  implements SpeechRecognizeListener {
+
+    private String serviceType = SpeechRecognizerClient.SERVICE_TYPE_WEB;
+    private SpeechRecognizerClient client;
+
+    private Activity activity = null;
+    private Context context = null;
+
+    SpeechToText(Context context, Activity activity) {
+
+        context = context;
+        activity = activity;
+        SpeechRecognizerManager.getInstance().initializeLibrary(context);
+    }
+
+    private void SpeechToText() {
+        if(PermissionUtils.checkAudioRecordPermission(activity)) {
+
+            SpeechRecognizerClient.Builder builder = new SpeechRecognizerClient.Builder().
+                    setServiceType(serviceType);
+
+            if (serviceType.equals(SpeechRecognizerClient.SERVICE_TYPE_WORD)) {
+            }
+
+            client = builder.build();
+
+            client.setSpeechRecognizeListener(this);
+            client.startRecording(true);
+        }
+    }
+
+    public void Release() {
+        // API를 더이상 사용하지 않을 때 finalizeLibrary()를 호출한다.
+        SpeechRecognizerManager.getInstance().finalizeLibrary();
+    }
+
+    @Override
+    public void onReady() {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int errorCode, String errorMsg) {
+        //TODO implement interface DaumSpeechRecognizeListener method
+        Log.e("SpeechSampleActivity", "onError");
+
+        client = null;
+    }
+
+    @Override
+    public void onPartialResult(String partialResult) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        final StringBuilder builder = new StringBuilder();
+        Log.i("SpeechSampleActivity", "onResults");
+
+        ArrayList<String> texts = results.getStringArrayList(SpeechRecognizerClient.KEY_RECOGNITION_RESULTS);
+        ArrayList<Integer> confs = results.getIntegerArrayList(SpeechRecognizerClient.KEY_CONFIDENCE_VALUES);
+
+        for (int i = 0; i < texts.size(); i++) {
+            builder.append(texts.get(i));
+            builder.append(" (");
+            builder.append(confs.get(i).intValue());
+            builder.append(")\n");
+        }
+    }
+
+    @Override
+    public void onAudioLevel(float audioLevel) {
+
+    }
+
+    @Override
+    public void onFinished() {
+
+    }
+}
